f8e5b267b08fd51e5c8147539b6b147979ae0492
vivek <vivekcse.in@gmail.com>
2025-09-13 19:41:50 +0530

    dependency versions updated

--- src/components/layouts/Hero.tsx
+++ src/components/layouts/Hero.tsx
@@ -3,7 +3,7 @@ import React from "react";
 const Hero = () => {
   return (
     <>
-      <h1>Next.js rontend Template for 2025</h1>
+      <h1>Next.js Frontend Template for 2025</h1>
     </>
   );
 };
